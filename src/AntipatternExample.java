e2f2f88b3788c9c7f5b97c4b4d613a719201379f
Santiago <juniorsantiagoalvarez03@gmail.com>
2025-03-02 00:11:49 -0500

    Código Ejecutable del Antipatron

---
+++ src/AntipatternExample.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class AntipatternExample {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/dictionary";
+    private static final String USER = "root";
+    private static final String PASSWORD = "santiish";
+
+    public void getWordsData() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM words");
+
+            while (resultSet.next()) {
+                System.out.println("Palabra: " + resultSet.getString("spanish"));
+            }
+            System.out.println("");
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPeopleData() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM people");
+
+            while (resultSet.next()) {
+                System.out.println("Email: " + resultSet.getString("email")); // Cambia "name" por el nombre de la columna en tu tabla
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        AntipatternExample example = new AntipatternExample();
+        example.getWordsData();
+        example.getPeopleData();
+    }
+}
