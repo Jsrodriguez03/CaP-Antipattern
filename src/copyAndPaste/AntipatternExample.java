bffdab242973515579ebd0cfe57bd5ebc5f551c8
Luis Pinto <dapin46@hotmail.com>
2025-03-02 21:19:53 -0500

    Refactorización de la función realizada

---
+++ src/copyAndPaste/AntipatternExample.java
@@ -0,0 +1,37 @@
+package copyAndPaste;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AntipatternExample {
+
+  private static final String URL = "jdbc:mysql://localhost:3306/dictionary";
+  private static final String USER = "luis";
+  private static final String PASSWORD = "Shayra11";
+
+  private void getData(String tableName, String columnName) {
+
+    try (
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName)
+      )
+    {
+      while (resultSet.next()) {
+        System.out.println(columnName + ": " + resultSet.getString(columnName));
+      }
+    } catch (SQLException e) {
+      System.out.println("Something went wrong: " + e.toString());
+    }
+    System.out.println("");
+  }
+
+  public static void main(String[] args) {
+    AntipatternExample example = new AntipatternExample();
+    example.getData("words", "kankuamo");
+    example.getData("people", "email");
+  }
+}
